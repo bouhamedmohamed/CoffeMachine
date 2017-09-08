7bd0753b8d256ebe3298b1a027a5b1c9d95d3b80
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-08 11:48:42 +0200

    Coffee machine should send chocolate  when we put H

--- src/main/java/domain/CoffeeMachine.java
+++ src/main/java/domain/CoffeeMachine.java
@@ -4,6 +4,13 @@ public class CoffeeMachine {
     public String sendCommand(String command) {
         if (command.equals(""))
             return "";
-        return "Drink maker makes 1 tea with no sugar therefore no stick";
+        String dringType = "";
+        final String[] commandParts = command.split(":");
+        final String commandDrinkType = commandParts[0];
+        if (commandDrinkType.equals("T"))
+            dringType = "tea";
+        else
+            dringType = "chocolate";
+        return "Drink maker makes 1 " + dringType + " with no sugar therefore no stick";
     }
 }
--- src/test/java/domain/CoffeeMachineTest.java
+++ src/test/java/domain/CoffeeMachineTest.java
@@ -13,4 +13,9 @@ public class CoffeeMachineTest {
     public void should_send_tea_when_we_put_T() {
         Assert.assertEquals("Drink maker makes 1 tea with no sugar therefore no stick",new CoffeeMachine().sendCommand("T::"));
     }
+
+    @Test
+    public void should_send_chocolate_when_we_put_H() {
+        Assert.assertEquals("Drink maker makes 1 chocolate with no sugar therefore no stick",new CoffeeMachine().sendCommand("H::"));
+    }
 }
